4c6ad5f5651904364811156363eeae3a780b9df6
GabrielMerinoUCA <gabriel.merino13079@est.uca.edu.ni>
2023-08-08 16:15:43 -0230

    Eleventh JS tutorial finished

---
+++ js/front-end/Javascript-tutorials-for-beginners/11-If-Statements.js
@@ -0,0 +1,14 @@
+var youLikeMeat = true;
+var myNum = 10;
+if (youLikeMeat) {
+    console.log("You like meat");
+}
+if(7 > 5) {
+    console.log("Indeed, 7 is greater than 5");
+}
+
+if(myNum == 10) {
+    console.log("It appears that 10 is indeed equal to 10");
+} else {
+    console.log("myNum is not equal to 10")
+}
